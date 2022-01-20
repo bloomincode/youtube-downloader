010e6768a153fa2536b3a5d649d6662f6253af3e
Georgina Nuthall <georgina.nuthall@gmail.com>
2022-01-20 17:48:31 +0000

    code review changes

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,27 +1,31 @@
 from pytube import YouTube
 
-def display_video_stats(yt):
-    length_mins = yt.length / 60
-    print("Title: %s" % yt.title)
-    print("Description - %s" % yt.description)
-    print("Rating: %.1f" % yt.rating)
-    print("Approx length of video: %d mins" % length_mins)
-    print("Number of views: %s" % yt.views)
-
-def get_high_quality_video(streams):
-    return streams.get_highest_resolution()
+def display_stats(yt):
+    video = {
+        "title": yt.title,
+        "description": yt.description,
+        "length": (yt.length / 60),
+        "views": yt.views
+    }
+    for key, value in video.items():
+        print(key, ":", value)
+        print("--------------")
 
 def download_video(video):
     print("Downloading..")
-    video.download()
-    print("Download completed")
+    try:
+        video.download(output_path="./youtube-downloader/")
+        print("Download finished")
+    except TimeoutError:
+        print("Download was taking too long!")
+        pass
 
 def main():
-    print("Enter link: ")
+    print("Enter YT Link: ")
     link = input()
     yt = YouTube(link)
-    display_video_stats(yt)
-    video = get_high_quality_video(yt.streams)
+    display_stats(yt)
+    video = yt.streams.get_highest_resolution()
     download_video(video)
 
 if __name__ == "__main__":
